272f1c51fe168ee168d85264e8985d38e78a73f3
Igor Goncharenko <igor.goncharenko@abtasty.com>
2026-03-16 13:24:50 +0100

    feat: initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,45 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // 1️⃣ Third-party packages (alphabetical automatically)
+            ['^@?\\w'],
+
+            // 2️⃣ Your @/ aliases
+            ['^@/'],
+
+            // 3️⃣ Relative imports
+            ['^\\.'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+      'react/jsx-sort-props': [
+        'warn',
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          ignoreCase: true,
+          noSortAlphabetically: false,
+        },
+      ],
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
