373ac8e7f7cea075cb649a597c50f1131d15bfc1
Igor Goncharenko <igor.goncharenko@abtasty.com>
2026-03-17 11:45:23 +0100

    feat: clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased ${inter.className}`}>{children}</body>
+      <body className={`antialiased ${inter.className}`}>
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
     </html>
   )
 }
