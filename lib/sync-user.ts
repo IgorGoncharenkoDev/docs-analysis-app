50a523663cf3549410eb46805049ee81b7e82c5a
Igor Goncharenko <igor.goncharenko@abtasty.com>
2026-03-19 16:56:30 +0100

    feat: syncing user from clerk to database

---
+++ lib/sync-user.ts
@@ -0,0 +1,49 @@
+import { currentUser } from '@clerk/nextjs/server'
+
+import { prisma } from '@/lib/db/prisma'
+import { DbUser } from '@/types/user'
+
+export async function syncUser(): Promise<DbUser> {
+  try {
+    const clerkUser = await currentUser()
+    if (!clerkUser) throw new Error('Unauthorized')
+
+    const email = clerkUser.emailAddresses[0]?.emailAddress
+    if (!email) throw new Error('No user email found')
+
+    // finding the user in db by the clerk user id
+    let dbUser = await prisma.user.findUnique({
+      where: {
+        clerkUserId: clerkUser.id,
+      },
+    })
+
+    // updating the existing user in db
+    if (dbUser) {
+      dbUser = await prisma.user.update({
+        where: {
+          id: dbUser.id,
+        },
+        data: {
+          email,
+          name: `${clerkUser.firstName || ''} ${clerkUser.lastName || ''}`,
+        },
+      } )
+    }
+    // creating a new user in db
+    else {
+      dbUser = await prisma.user.create({
+        data: {
+          clerkUserId: clerkUser.id,
+          email,
+          name: `${clerkUser.firstName || ''} ${clerkUser.lastName || ''}`,
+        },
+      } )
+    }
+
+    return dbUser
+  } catch (error) {
+    console.error('Error syncing user from Clerk: ', error || 'Unknown error')
+    throw error
+  }
+}
